10bd0a42d514511d64b7237d4df50e9274d3a035
u1921415 <jonathan.gibbons@warwick.ac.uk>
2023-12-04 17:06:10 +0000

    Error still present in fitting quotient

--- set1/single_byte_xor_cypher/src/main.rs
+++ set1/single_byte_xor_cypher/src/main.rs
@@ -59,9 +59,28 @@ pub fn calculate_character_counts(stream: &Vec<u8>) -> HashMap<u8, f32> {
     return char_counts;
 }
 
-// fn calculate_frequency_quotient(char_counts: &HashMap<u8, f32>) -> f32 {
-    
-// }
+fn calculate_fitting_quotient(stream: &Vec<u8>) -> f32{
+    let b = stream.as_slice();
+
+    // If bytes are not ascii characters, return maximum value.
+    if !b.is_ascii() {
+        return std::f32::MAX;
+    } else {
+        let char_counts = calculate_character_counts(stream);
+        let length = b.len() as f32;
+
+        // Calculate the fitting quotient
+        let fitting_quotient = ETAOIN_SHRDLU.entries.iter().fold(0.0, |a, &(c, frequency)| {
+            let expected_count = frequency / 100.0 * length;
+            let actual_count = char_counts.get(&c).unwrap_or(&0.0);
+            a + (expected_count - actual_count).powi(2)
+        });
+
+        return fitting_quotient;
+    }
+
+
+}
 
 fn main() {
     // Encoded text given by the challenge.
@@ -74,16 +93,34 @@ fn main() {
     };
 
     let mut lowest_fq = 0.0;
+    let mut cipher_key = 0;
+    let mut text: Vec<u8> = vec!(0);
 
     for key in 0..=255 {
         // Perform the single byte XOR.
         let decipher_attempt = single_byte_xor(&bytes, key);
 
         // Find the occurances of character in the deciphered stream.
-        let char_counts: HashMap<u8, f32> = calculate_character_counts(&decipher_attempt);
+        // let char_counts: HashMap<u8, f32> = calculate_character_counts(&decipher_attempt);
+        
+        // Iterate over the bytes of byte stream. 
+        // If alphabetic or compare with ETAOIN_SHRDLU
+        // If space, tab, newline do nothing.
+        // If control character, return large value
+        // if none of above, 
+        let fitting_quotient = calculate_fitting_quotient(&decipher_attempt);
 
-        // // Compare expected frequency against actual frequency
-        // let fq: f32 = calculate_frequency_quotient(&char_counts);
+        if fitting_quotient < lowest_fq || lowest_fq == 0.0 {
+            lowest_fq = fitting_quotient;
+            cipher_key = key;
+            text = decipher_attempt;
+        }
+        
     }
+
+    println!("Key: {:x}, fitting quotient {}", cipher_key, lowest_fq);
+    let deciphered_text_chars: Vec<char> = single_byte_xor(&text, cipher_key).iter().map(|&byte| byte as char).collect();
+    let deciphered_text: String = deciphered_text_chars.into_iter().collect();
+    println!("{}", deciphered_text);
     
 }
