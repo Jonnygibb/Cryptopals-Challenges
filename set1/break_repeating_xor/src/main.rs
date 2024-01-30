d79d5e9d3617520177baa130efbdc5272eb4b088
Jonathan Gibbons <jgibbo10@jaguarlandrover.com>
2024-01-30 16:57:46 +0000

    Incorporated single byte xor code to decipher chunks

--- set1/break_repeating_xor/src/main.rs
+++ set1/break_repeating_xor/src/main.rs
@@ -1,6 +1,57 @@
 use std::fs;
+use phf::phf_map;
 use std::cmp::Ordering;
 use base64::{engine::general_purpose::STANDARD, Engine as _};
+
+// Frequency of letter appearance in concise oxford dictionary(9th edition, 1995).
+static EXPECTED_FREQUENCY: phf::Map<u8, f32> = phf_map! {
+    b'e' => 11.1607,
+    b'a' => 8.4966,
+    b'r' => 7.5809,
+    b'i' => 7.5448,
+    b'o' => 7.1635,
+    b't' => 6.9509,
+    b'n' => 6.6544,
+    b's' => 5.7351,
+    b'l' => 5.4893,
+    b'c' => 4.5388,
+    b'u' => 3.6308,
+    b'd' => 3.3844,
+    b'p' => 3.1671,
+    b'm' => 3.0129,
+    b'h' => 3.0034,
+    b'g' => 2.4705,
+    b'b' => 2.0720,
+    b'f' => 1.8121,
+    b'y' => 1.7779,
+    b'w' => 1.2899,
+    b'k' => 1.1016,
+    b'v' => 1.0074,
+    b'x' => 0.2902,
+    b'z' => 0.2722,
+    b'j' => 0.1965,
+    b'q' => 0.1962,
+};
+
+pub fn xor_by_single_byte(stream: &[u8], byte: u8) -> Vec<u8>{
+    return stream.iter().map(|b| b ^ byte).collect();
+}
+
+pub fn is_alphabetic(byte: u8) -> bool {
+    (byte >= 0x41 && byte <= 0x5A) || (byte >= 0x61 && byte <= 0x7A)
+}
+
+pub fn score_byte(byte: u8) -> f32 {
+    if is_alphabetic(byte) {
+        return match EXPECTED_FREQUENCY.get(&byte) {
+            Some(frequency) => *frequency,
+            None => 0.0,
+        };
+    } else {
+        return 0.0;
+    }
+}
+
 
 pub fn read_and_decode_file(filename: &str) -> Result<Vec<u8>, std::io::Error> {
     // Read the contents of the file into a Vec<u8>.
@@ -76,6 +127,50 @@ pub fn find_likeliest_keysize(encoded_message: &[u8]) -> u8 {
     return key_scores[0].0;
 }
 
+pub fn transpose(encoded_message: &[u8]) -> Vec<Vec<u8>> {
+    return (0..29).map(|i| encoded_message.iter().skip(i).step_by(29).cloned().collect()).collect();
+}
+
+pub fn break_single_char_xor(message_chunk: &[u8]) -> u8 {
+    // Estblish variables to hold best scoring key and string.
+    let mut cipher_key: u8 = 0;
+    let mut highest_score: f32 = 0.0;
+
+    // Iterate through all the XOR keys and score the resulting stream.
+    for key in 0..=255 {
+
+        // Try a key from the set of all one byte keys.
+        let decipher_attempt = xor_by_single_byte(message_chunk, key);
+
+        // Establish a variable to hold the score.
+        let mut total_score = 0.0;
+
+        for byte in &decipher_attempt {
+            total_score += score_byte(*byte);
+        }
+
+        //Compare the score of each window with the best score so far.
+        if total_score > highest_score {
+            cipher_key = key;
+            highest_score = total_score;
+        }
+    }
+
+    // Print the best scoring string from the input data.
+    println!("key: {}", cipher_key as char);
+    return cipher_key;
+}
+
+pub fn break_repeating_xor(encoded_message: &[u8], keysize: &u8) {
+    let chunked_message = transpose(encoded_message);
+
+    for i in 0..*keysize {
+        let key = break_single_char_xor(&chunked_message[i as usize]);
+        println!("{}", key);
+    }
+
+}
+
 fn main() {
 
     // Test to see whether hamming_distance passes the test set in the challenge.
@@ -91,6 +186,6 @@ fn main() {
 
     let keysize = find_likeliest_keysize(&encoded_message);
 
-    println!("{}", keysize);
+    let encryption_key = break_repeating_xor(&encoded_message, &keysize);
     
 }
